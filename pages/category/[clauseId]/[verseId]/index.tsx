2d0c166db7fd9f204a4afa58152d656dd855bc5d
Ikhsan Firdauz <ikhsan.firdauz@dev.accelist.com>
2023-11-15 17:56:31 +0700

    ui category progress 4

--- pages/category/[clauseId]/[verseId]/index.tsx
+++ pages/category/[clauseId]/[verseId]/index.tsx
@@ -3,6 +3,8 @@ import { Title } from '../../../../components/Title';
 import { Page } from '../../../../types/Page';
 import { WithCategoryLayout } from '@/components/CategoryLayout';
 import { Select } from 'antd';
+import { CategoryUploadedFileView } from '@/components/CategoryUploadedFile';
+import { AButton } from '@/components/Buttons';
 
 interface selectType {
     value: string;
@@ -29,18 +31,37 @@ const selectOptions: selectType[] = [
     },
 ]
 
+const dummyFiles = ['file-1.pdf', 'file-2.png']
+
 const VersePage: Page = () => {
     return (
         <div>
             <Title>Ayat</Title>
             <div className='flex'>
                 <Select
-                    className='w-56 rounded-none'
+                    className='w-52 rounded-none'
                     defaultValue={selectOptions[0]?.label}
                     options={selectOptions} 
                 />
-                <div className='bg-blue-300 flex flex-1'>
-
+                <div className='flex-1 mx-5'>
+                    <div className='text-base'>
+                        Apakah Anda dapat menunjukkan bahwa subjek data pribadi telah menyetujui pemrosesan data mereka?
+                    </div>
+                    <div className='flex mt-6'>
+                        <div className='bg-red-300 flex flex-1'>
+                            {
+                                dummyFiles.map((file, i) => 
+                                <div className='mr-8' key={i}>
+                                    <CategoryUploadedFileView filename={file}/>
+                                </div>
+                                )
+                            }
+                        </div>
+                        <div>
+                            <AButton text='+ Upload File'/>
+                            {/* <button className='border-[#4F7471] border-[3px] text-[#4F7471] font-semibold'>+ Upload</button> */}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
