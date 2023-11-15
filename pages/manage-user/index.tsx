425943ff712492db3074d7c871349ac096584558
Michelle_AG27 <michelle.guntjoro@gmail.com>
2023-11-15 10:45:51 +0700

    Fix Modal

--- pages/manage-user/index.tsx
+++ pages/manage-user/index.tsx
@@ -6,16 +6,15 @@ import { useFetchWithAccessToken } from '@/functions/useFetchWithAccessToken';
 import PopupAddNewUser from '@/components/manage-user/PopupAddNewUser';
 import { AddNewUserFormProps } from '@/components/interfaces/AddNewUserForms';
 import { BackendApiUrl } from '@/functions/BackendApiUrl';
-import { Page } from '@/types/Page';
 import { useRouter } from 'next/router';
 import { listCurrentRole } from '@/components/constants';
-import { WithDefaultLayout } from '@/components/DefautLayout';
 import { InputAddNewUserForm } from '@/components/common/input/InputAddNewUserForm';
 import { InputSelectAddNewUserForm } from '@/components/common/input/InputSelectAddNewUserForm';
 import { useController } from 'react-hook-form';
 import { SelectOptions } from '@/components/interfaces/AddNewUserForms';
 import useSWR from 'swr';
 import { useSwrFetcherWithAccessToken } from '@/functions/useSwrFetcherWithAccessToken';
+import { Modal } from 'antd';
 
 interface DataItem {
     roleName: string;
@@ -31,15 +30,16 @@ const schema: ZodType<AddNewUserFormProps> = z.object({
     password: z.string({ required_error: 'Password can\'t be empty' }),
     confirmPassword: z.string({ required_error: 'Confirmation password can\'t be empty' }),
     role: z.number({ required_error: 'Role can\'t be empty' }).min(1, 'Role can\'t be empty'),
-    currentRole: z.string({ required_error: 'Current Role can\'t be empty' }), // Add this line
+    currentRole: z.string({ required_error: 'Current Role can\'t be empty' }),
 });
 
-const AddNewUserPage: Page = () => {
+const AddNewUserPage: React.FC = () => {
     const { replace } = useRouter();
     const { fetchPOST } = useFetchWithAccessToken();
     const [showPopupSuccess, setShowPopupSuccess] = useState(false);
-    const { register, handleSubmit, reset, control, formState: { errors } } = useForm<AddNewUserFormProps>({
+    const { register, handleSubmit, reset, control, formState: { errors, isValid} } = useForm<AddNewUserFormProps>({
         resolver: zodResolver(schema),
+        mode: 'onChange',
     });
 
     const { field: fieldCurrentRole } = useController({ name: 'currentRole', control });
@@ -54,7 +54,6 @@ const AddNewUserPage: Page = () => {
             if (!data) {
                 return [];
             }
-
             const options = data.map((item) => ({
                 label: item.roleName,
                 value: item.roleName,
@@ -67,9 +66,16 @@ const AddNewUserPage: Page = () => {
     }, [data]);
 
     const onSubmit = async (formData: AddNewUserFormProps) => {
+        console.log(1)
+        if (!isValid) {
+            console.log(2)
+            return;
+        }
+
         const payload = {
             ...formData,
         };
+        console.log("test", formData)
         const { data } = await fetchPOST<AddNewUserFormResponse>(BackendApiUrl.addNewUser, payload);
 
         if (data?.response) {
@@ -87,56 +93,62 @@ const AddNewUserPage: Page = () => {
                 })
             }} />
         }
-        return null
+        return null;
     }
 
     return (
-        <div className="flex flex-col px-6 md:px-12 lg:px-36 mt-10 md:mt-16">
-            <h3 className="text-xl sm:text-2xl text-center font-body font-bold mt-4 sm:mt-6 mb-4 sm:mb-8">Add New Account</h3>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <InputAddNewUserForm
-                    id={'fullName'}
-                    label='Name'
-                    register={register('fullName', { required: true })}
-                    placeholder='Insert Name'
-                    formErrorMessage={errors.fullName?.message}
-                />
-                <InputAddNewUserForm
-                    id={'email'}
-                    label='Email'
-                    register={register('email', { required: true })}
-                    placeholder='Insert Email'
-                    formErrorMessage={errors.email?.message}
-                />
-                <InputAddNewUserForm
-                    id={'password'}
-                    label='Password'
-                    register={register('password', { required: true })}
-                    placeholder='Insert Password'
-                    formErrorMessage={errors.password?.message}
-                />
-                <InputAddNewUserForm
-                    id={'password'}
-                    label='Confirm Password'
-                    register={register('password', { required: true })}
-                    placeholder='Insert Confirmation Password'
-                    formErrorMessage={errors.confirmPassword?.message}
-                />
-                <InputSelectAddNewUserForm
-                    label='Role'
-                    value={listCurrentRole.find(e => e.value === fieldCurrentRole.value) ?? ''}
-                    name={fieldCurrentRole.name}
-                    options={roleOptions}
-                    onChange={(selectedOptions: SelectOptions<string>) => fieldCurrentRole.onChange(selectedOptions.value)}
-                    placeholder='Choose Role'
-                    formErrorMessage={errors?.currentRole?.message}
-                />
-            </form>
-            <button onClick={() => setShowPopupSuccess(true)} className="addButton">Add</button>
-            {renderPopupSuccess()}
-        </div>
+        <Modal
+            open={true}
+            centered
+            footer={[<button key={2} onClick={() => setShowPopupSuccess(true)} className="addButton">Add</button>]}
+            width={800}
+        >
+            <div>
+                <div className="flex flex-col px-2 md:px-4 lg:px-8 mt-4 md:mt-16">
+                <h3 className="text-xl sm:text-2xl text-center font-body font-bold mt-4 sm:mt-6 mb-4 sm:mb-8">Add New Account</h3>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <InputAddNewUserForm
+                        id={'fullName'}
+                        label='Name'
+                        register={register('fullName', { required: true })}
+                        placeholder='Insert Name'
+                        formErrorMessage={errors.fullName?.message}
+                    />
+                    <InputAddNewUserForm
+                        id={'email'}
+                        label='Email'
+                        register={register('email', { required: true })}
+                        placeholder='Insert Email'
+                        formErrorMessage={errors.email?.message}
+                    />
+                    <InputAddNewUserForm
+                        id={'password'}
+                        label='Password'
+                        register={register('password', { required: true })}
+                        placeholder='Insert Password'
+                        formErrorMessage={errors.password?.message}
+                    />
+                    <InputAddNewUserForm
+                        id={'confirmPassword'}
+                        label='Confirm Password'
+                        register={register('password', { required: true })}
+                        placeholder='Insert Confirmation Password'
+                        formErrorMessage={errors.confirmPassword?.message}
+                    />
+                    <InputSelectAddNewUserForm
+                        label='Role'
+                        value={listCurrentRole.find(e => e.value === fieldCurrentRole.value) ?? ''}
+                        name={fieldCurrentRole.name}
+                        options={roleOptions}
+                        onChange={(selectedOptions: SelectOptions<string>) => fieldCurrentRole.onChange(selectedOptions.value)}
+                        placeholder='Choose Role'
+                        formErrorMessage={errors?.currentRole?.message}
+                    />
+                </form>
+                {renderPopupSuccess()}
+            </div>
+            </div>
+        </Modal>
     );
 };
-
-AddNewUserPage.layout = WithDefaultLayout;
 export default AddNewUserPage;
