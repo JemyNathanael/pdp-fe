8613c600eedf83bf4acac69c6696fbbac50ae863
Ikhsan Firdauz <ikhsan.firdauz@dev.accelist.com>
2023-11-15 14:11:35 +0700

    ui category progress 3

--- pages/category/[clauseId]/[verseId]/index.tsx
+++ pages/category/[clauseId]/[verseId]/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React from 'react';
 import { Title } from '../../../../components/Title';
 import { Page } from '../../../../types/Page';
 import { WithCategoryLayout } from '@/components/CategoryLayout';
