7f189938f18276de7459e7c20be462151bdae779
OtakKentung <95601654+vannessiwata@users.noreply.github.com>
2023-11-20 11:09:39 +0700

    Add floating button

--- pages/checklist/index.tsx
+++ pages/checklist/index.tsx
@@ -5,6 +5,10 @@ import { v4 as uuidv4 } from 'uuid';
 //import useSwr from 'swr';
 //import { useSwrFetcherWithAccessToken } from '@/functions/useSwrFetcherWithAccessToken';
 import { FaFilePdf, FaFileWord, FaFileImage, FaTimes } from 'react-icons/fa';
+import { WithDefaultLayout } from '@/components/DefautLayout';
+import { ConfigProvider, FloatButton } from 'antd';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowsRotate, faBars, faList, faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
 //import { LoadingOverlay } from "@/components/LoadingOverlay";
 
 const ChecklistPage = () => {
@@ -14,15 +18,15 @@ const ChecklistPage = () => {
     const [formData, setFormData] = useState({});
 
     const handleChange = (fieldName, value) => {
-    setFormData({ ...formData, [fieldName]: value });
+        setFormData({ ...formData, [fieldName]: value });
     };
 
     const data = [
-        { 
-            id: 'guid1', 
-            description: 'Apakah Anda dapat menunjukkan bahwa subjek data pribadi telah menyetujui pemrosesan data mereka?', 
+        {
+            id: 'guid1',
+            description: 'Apakah Anda dapat menunjukkan bahwa subjek data pribadi telah menyetujui pemrosesan data mereka?',
             uploadStatusId: '1',
-            blobs:[
+            blobs: [
                 {
                     id: 'guid1',
                     fileName: 'File1.pdf',
@@ -43,11 +47,11 @@ const ChecklistPage = () => {
                 }
             ]
         },
-        { 
-            id: 'guid2', 
+        {
+            id: 'guid2',
             description: 'Apakah permintaan persetujuan dapat dibedakan dengan jelas dari hal-hal lain, dengan cara yang dapat dimengerti dan dalam bentuk yang mudah diakses, dan ditulis dalam bahasa yang jelas dan lugas?',
             uploadStatusId: '2',
-            blobs:[
+            blobs: [
                 {
                     id: 'guid1',
                     fileName: 'File1.pdf',
@@ -102,11 +106,16 @@ const ChecklistPage = () => {
     // }
 
     return (
-        <div>
+        <ConfigProvider
+            theme={{
+                token: {
+                    colorPrimary: '#4F7471',
+                }
+            }}>
             {data?.map((row) => (
                 <div className='checklistRow' key={row.id}>
                     <div className='checklistRow' key={row.id}>
-                        <div className='checklistColumn' style={{ width: '20%'}}>
+                        <div className='checklistColumn' style={{ width: '20%' }}>
                             <select defaultValue={row.uploadStatusId} onChange={(e) => handleChange('dropdownField', e.target.value)}>
                                 <option key='0' value='0'>
                                     Pilih status..
@@ -125,12 +134,12 @@ const ChecklistPage = () => {
                                 </option>
                             </select>
                         </div>
-                        <div className='checklistColumn' style={{ width: '60%'}}>
+                        <div className='checklistColumn' style={{ width: '60%' }}>
                             <div className='checklistRow'>
                                 <label>{row.description}</label>
                             </div>
                             <div className='checklistRow'>
-                                <div className='checklistColumn' style={{ width: '20%'}}>
+                                <div className='checklistColumn' style={{ width: '20%' }}>
                                     <input
                                         type="file"
                                         multiple
@@ -142,37 +151,37 @@ const ChecklistPage = () => {
                                 </div>
                             </div>
                             <div className='checklistRow'>
-                                    {row.blobs.length > 0 && (
-                                        <div>
-                                            <div className="file-list">
-                                                {row.blobs.map((blob) => (
-                                                    <div key={blob.id} className="file-item">
-                                                        {/* File type icon */}
-                                                        <FaTimes className="delete-button" onClick={() => deleteFile(blob.fileName)} />
-                                                        {blob.contentType === 'pdf' && <FaFilePdf color="#537372" size={30} />}
-                                                        {blob.contentType === 'docx' && <FaFileWord color="#537372" size={30} />}
-                                                        {blob.contentType === 'image' && <FaFileImage color="#537372" size={30} />}
-                                                        {/* File name */}
-                                                        <p>{blob.fileName}</p>
-                                                        {/* Delete button */}
-                                                        
-                                                    </div>
-                                                ))}
-                                                {selectedFiles.map((file, index) => (
-                                                    <div key={index} className="file-item">
-                                                        {/* File type icon */}
-                                                        <FaTimes className="delete-button" onClick={() => deleteFile(file.name)} />
-                                                        {file.type === 'pdf' && <FaFilePdf color="#537372" size={30} />}
-                                                        {file.type === 'docx' && <FaFileWord color="#537372" size={30} />}
-                                                        {file.type === 'image' && <FaFileImage color="#537372" size={30} />}
-                                                        {/* File name */}
-                                                        <p>{file.name}</p>
-                                                        {/* Delete button */}
-                                                        
-                                                    </div>
-                                                ))}
-                                            </div>
-                                            {/* <ul>
+                                {row.blobs.length > 0 && (
+                                    <div>
+                                        <div className="file-list">
+                                            {row.blobs.map((blob) => (
+                                                <div key={blob.id} className="file-item">
+                                                    {/* File type icon */}
+                                                    <FaTimes className="delete-button" onClick={() => deleteFile(blob.fileName)} />
+                                                    {blob.contentType === 'pdf' && <FaFilePdf color="#537372" size={30} />}
+                                                    {blob.contentType === 'docx' && <FaFileWord color="#537372" size={30} />}
+                                                    {blob.contentType === 'image' && <FaFileImage color="#537372" size={30} />}
+                                                    {/* File name */}
+                                                    <p>{blob.fileName}</p>
+                                                    {/* Delete button */}
+
+                                                </div>
+                                            ))}
+                                            {selectedFiles.map((file, index) => (
+                                                <div key={index} className="file-item">
+                                                    {/* File type icon */}
+                                                    <FaTimes className="delete-button" onClick={() => deleteFile(file.name)} />
+                                                    {file.type === 'pdf' && <FaFilePdf color="#537372" size={30} />}
+                                                    {file.type === 'docx' && <FaFileWord color="#537372" size={30} />}
+                                                    {file.type === 'image' && <FaFileImage color="#537372" size={30} />}
+                                                    {/* File name */}
+                                                    <p>{file.name}</p>
+                                                    {/* Delete button */}
+
+                                                </div>
+                                            ))}
+                                        </div>
+                                        {/* <ul>
                                                 {selectedFiles.map((file, index) => (
                                                 <li key={index}>
                                                     <img
@@ -184,35 +193,47 @@ const ChecklistPage = () => {
                                                 </li>
                                                 ))}
                                             </ul> */}
-                                        </div>
-                                    )}
-                                </div>
+                                    </div>
+                                )}
+                            </div>
                         </div>
                     </div>
-                    
+
                 </div>
             ))}
             {/* Add a button to submit the form with formData to your backend */}
+            <div className=""></div>
+            <FloatButton.Group
+                trigger="click"
+                type="primary"
+                className='m-5'
+                icon={<FontAwesomeIcon icon={faBars} />}
+            >
+                <FloatButton type='primary' icon={<FontAwesomeIcon icon={faArrowsRotate} />} tooltip="Update Pasal" />
+                <FloatButton type='primary' icon={<FontAwesomeIcon icon={faPlus} />} tooltip="Add Pasal" />
+                <FloatButton type='primary' icon={<FontAwesomeIcon icon={faMinus} />} tooltip="Delete" />
+            </FloatButton.Group>
             <button className='roundedRectangleButton' onClick={() => console.log('Submit:', formData)}>Save</button>
-        </div>
+        </ConfigProvider>
     );
 };
 
+ChecklistPage.layout = WithDefaultLayout
 export default ChecklistPage;
 
-export interface ChecklistResponse{
+export interface ChecklistResponse {
     checklistList: ChecklistList[];
     successStatus: boolean;
 }
 
-export interface ChecklistList{
+export interface ChecklistList {
     id: uuidv4;
     description: string;
     uploadStatusId: string;
     blobs: BlobList[];
 }
 
-export interface BlobList{
+export interface BlobList {
     id: uuidv4;
     fileName: string;
     filePath: string;
