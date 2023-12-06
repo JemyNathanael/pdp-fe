e246afd881d684cdeee191d69985b410302c71dc
Ardyanto Konstantin <ardy.konstantin02@gmail.com>
2023-12-06 17:24:59 +0700

    fixed bug number 9

--- components/category/CategoryVerseContent.tsx
+++ components/category/CategoryVerseContent.tsx
@@ -181,7 +181,7 @@ export const CategoryVerseContent: React.FC<CategoryVerseContentProps> = ({ chec
                         <div className='text-base flex items-center'>
                             <Dropdown menu={{ items }} trigger={canUpdateStatus ? ['contextMenu'] : []}>
                                 <div className='py-1'>
-                                    <p style={{ whiteSpace: 'pre-line' }}>{title}</p>
+                                    <p style={{ whiteSpace: 'pre-line' , textAlign: 'justify'}}>{title}</p>
                                 </div>
                             </Dropdown>
                             {canUpdateStatus &&
