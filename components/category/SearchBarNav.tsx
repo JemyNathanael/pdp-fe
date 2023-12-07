1e1b6b8ae8359c6386df55d303bc281d60517104
crisl <reavleaz@yahoo.co.id>
2023-12-07 15:37:03 +0700

    Success direct page

--- components/category/SearchBarNav.tsx
+++ components/category/SearchBarNav.tsx
@@ -12,20 +12,41 @@ const SearchBarNav : React.FC<{placeholder: string, style: React.CSSProperties }
 
     const handleSearch = async (newValue: string) => {
         const searchApiUrl = `${BackendApiUrl.getHomeSearch}?search=${newValue}`;
-        try {
-            const response = await swrFetcher(searchApiUrl);
-            const options = response?.map((item) => ({value: item.id, label: item.title, type: item.type}));
-            console.log(options);
-            setData(options || []);
-        }
-        catch (error) {
-            console.log(error);
+        if(newValue != '') {
+            try {
+                const response = await swrFetcher(searchApiUrl);
+                const options = response?.map((item) => ({
+                    value: item.id, 
+                    label: item.title, 
+                    type: item['type'],
+                    firstSubCategoryId: item['firstSubCategoryId'],
+                    secondSubCategoryId: item['secondSubCategoryId'],
+                }));
+                console.log(options);
+                setData(options || []);
+            }
+            catch (error) {
+                console.log(error);
+            }
         }
     };
 
     const handleChange = (newValue: string) => {
         setValue(newValue);
     }
+
+    const handleSelect = (selectedValue, selectedData) => {
+        console.log("wtf ", selectedValue);
+        console.log("wtf asdsad", selectedData);
+        if(selectedData.type == 'Category') {
+            window.location.href = `/${selectedData.value}`;
+        } else if(selectedData.type == 'First Sub-Category') {
+            window.location.href = `/${selectedData.value}/${selectedData.firstSubCategoryId}`;
+        } else if(selectedData.type == 'Second Sub-Category') {
+            window.location.href = `/${selectedData.value}/${selectedData.firstSubCategoryId}/${selectedData.secondSubCategoryId}`;
+        }
+    }
+
 
     return (
         <Select
@@ -38,9 +59,14 @@ const SearchBarNav : React.FC<{placeholder: string, style: React.CSSProperties }
             filterOption={false}
             onSearch={handleSearch}
             onChange={handleChange}
+            onSelect={handleSelect}
             options={(data || []).map((d) => ({ 
+                key: `${d.value}-${d.label}`,
                 label: d.label,
                 value: d.value,
+                type: d['type'],
+                firstSubCategoryId: d['firstSubCategoryId'],
+                secondSubCategoryId: d['secondSubCategoryId'],
             }))}
         />
     )
