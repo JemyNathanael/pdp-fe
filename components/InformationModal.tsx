ec7367a2c349f529554c873f2ec3b18ae9a61b0e
Michelle_AG27 <michelle.guntjoro@gmail.com>
2023-12-08 12:45:13 +0700

    Make text Justify

--- components/InformationModal.tsx
+++ components/InformationModal.tsx
@@ -42,8 +42,8 @@ const InformationModal: React.FC<InformationModalProps> = ({ onCancel, visible, 
             informationData.map((info, i) => (
               <div key={i}>
                 <h4 className='text-md sm:text-lg font-body font-bold mb-2 sm:mb-3'>{info.title}</h4>
-                <p className='sm:text-md font-body mb-2 sm:mb-3'>{info.description}</p>
-                <ol type='1' className='sm:text-md font-body mb-2 sm:mb-3'>
+                <p className='sm:text-md font-body mb-2 sm:mb-3 text-justify'>{info.description}</p>
+                <ol type='1' className='sm:text-md font-body mb-2 sm:mb-3 text-justify'>
                   {info.verses?.map((verse, q) => (
                     <li className='mb-3' key={`${i}-${q}`}> {`(${q + 1})  ${verse.description}`}</li>
                   ))}
