3c26acded5091508c74f1d86ca14ed3c8b71be77
vasthytanisya <vrezkitanisya26@gmail.com>
2023-12-11 10:47:01 +0700

    add bg color popup

--- components/AddChecklistModal.tsx
+++ components/AddChecklistModal.tsx
@@ -22,8 +22,8 @@ interface AddChecklistResponse {
 
 const SuccessAddModal: React.FC<SuccessModalProps> = ({ onGoToHome }) => {
     return (
-        <div className="fixed inset-0 flex items-center justify-center bg-opacity-10 bg-secondary-100 backdrop-filter backdrop-blur-md" onClick={onGoToHome}>
-            <div className="flex flex-col p-6 sm:p-12 border items-center justify-center">
+        <div className="fixed inset-0 flex items-center justify-center backdrop-filter backdrop-blur-md" onClick={onGoToHome}>
+            <div className="flex flex-col p-6 sm:p-12 border items-center justify-center bg-white">
                 <FontAwesomeIcon icon={faCircleCheck} style={{ color: "#3788FD", fontSize: "64px", marginBottom: "8px" }} />
                 <div className="w-full h-4 sm:h-8" />
                 <h3 className="text-xl sm:text-2xl text-accent-100 font-body font-bold mt-4 sm:mt-6 mb-4 sm:mb-8">Successfully Added Checklist!</h3>
